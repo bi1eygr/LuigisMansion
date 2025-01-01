73fb9bb4fd52c593a439b5f077a562ace0c3bb12
bi1eygr <joseadastra@gmail.com>
2025-01-01 18:08:51 -0500

    Streaming a page p2

--- LemonLime/nextjs-dashboard/app/dashboard/loading.tsx
+++ LemonLime/nextjs-dashboard/app/dashboard/loading.tsx
@@ -1,3 +1,5 @@
 export default function Loading() { 
-  return <div>Loading...</div>;
+  return <div>Loading... loading.tsx is a special Next.js file 
+  built on top of Suspense, it allows you to create fallback 
+  UI to show as a replacement while page content loads.</div>;
 }

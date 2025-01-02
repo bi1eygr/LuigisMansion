dd30b733f28a3d3893538feb14c4b275a6098309
bi1eygr <joseadastra@gmail.com>
2025-01-02 17:47:51 -0500

    Dragonball Z lol goku why are you like this

--- LemonLime/nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ LemonLime/nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -3,16 +3,11 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data'; // Remove fetchLatestInvoices
-import { 
-  fetchLatestInvoices,
-  fetchCardData, 
-} from '@/app/lib/data';
 import { Suspense } from 'react';
-import { 
+import {
   RevenueChartSkeleton,
-  LatestInvoicesSkeleton, 
+  LatestInvoicesSkeleton,
 } from '@/app/ui/skeletons';
-
  
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();

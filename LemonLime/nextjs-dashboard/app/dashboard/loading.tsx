71a42af447f978fb8e4aef1d5982373fd31c16e7
bi1eygr <joseadastra@gmail.com>
2025-01-02 16:24:36 -0500

    Altering loading.tsx to display dashboard skeleton

--- LemonLime/nextjs-dashboard/app/dashboard/loading.tsx
+++ LemonLime/nextjs-dashboard/app/dashboard/loading.tsx
@@ -1,6 +1,12 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+
 export default function Loading() { 
+  return <DashboardSkeleton />; 
+
+  /**
   return <div>Loading... loading.tsx is a special Next.js file 
   built on top of Suspense, it allows you to create fallback 
   UI to show as a replacement while page content loads.Since SideNav
    while the dynamic content is loading.</div>;
+  */
 }

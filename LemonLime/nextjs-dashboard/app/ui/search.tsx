87c468a4f53ad92ca71c70012e3ca9d99565ff59
bi1eygr <joseadastra@gmail.com>
2025-01-05 13:38:00 -0500

    debounced callbacks Chapter11

--- LemonLime/nextjs-dashboard/app/ui/search.tsx
+++ LemonLime/nextjs-dashboard/app/ui/search.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
@@ -8,15 +9,17 @@ export default function Search() {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-    function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
+const handleSearch = useDebouncedCallback((term) => {
+  console.log(`Searching... ${term}`);
+ 
+  const params = new URLSearchParams(searchParams);
+  if (term) {
+    params.set('query', term);
+  } else {
+    params.delete('query');
   }
+  replace(`${pathname}?${params.toString()}`);
+}, 300);
 
 
   return (
@@ -30,8 +33,9 @@ export default function Search() {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
-}
+

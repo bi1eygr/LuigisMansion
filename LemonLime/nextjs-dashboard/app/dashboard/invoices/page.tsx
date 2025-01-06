37422ca7aeeb11d7d39816f35216e9895a461296
bi1eygr <joseadastra@gmail.com>
2025-01-05 22:34:28 -0500

    Revert "adding search functionality and omptimizing"
    This reverts commit 55db0886e4e4b4b37feff0877ba8a1753ab12ac3.

--- LemonLime/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ LemonLime/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -6,15 +6,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
  
-export default async function Page(props: {
-  searchParams?: Promise<{
-    query?: string;
-    page?: string;
-  }>;
-}) {
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
+export default async function Page() {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -24,7 +16,7 @@ export default async function Page(props: {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense> */}
       <div className="mt-5 flex w-full justify-center">

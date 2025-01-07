e2d0ee656e5b7d2e9b87f2f405fdb47716591106
bi1eygr <joseadastra@gmail.com>
2025-01-07 15:52:24 -0500

    Div change

--- LemonLime/nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ LemonLime/nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -20,10 +20,7 @@ export default function EditInvoiceForm({
 }) { 
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
  
-  return <form action={updateInvoiceWithId}></form>;
-}
-  return (
-    <form>
+  return <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">

b15131478c6f5d87ea887f47b7bcf7758bf1eb36
bi1eygr <joseadastra@gmail.com>
2025-01-08 11:16:08 -0500

    Adding error handling just in case Broly manages to get that neclace off.

--- LemonLime/nextjs-dashboard/app/lib/actions.ts
+++ LemonLime/nextjs-dashboard/app/lib/actions.ts
@@ -27,11 +27,16 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
+  
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    return { message: 'Database Error: Get Wrecked, Loser.'};
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -45,16 +50,27 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-  
+
+  try {
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-
+  } catch (error) {
+    return { 
+      message: 'Database Error: Get Wrecked, Loser.',
+    };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
+  try {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+  return {message: 'Invoice Disentegrated.'};
+} catch (error) {
+  return { message: 'Database Error: Fix Me or Perish, Fool.'};
+  }
 }

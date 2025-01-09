894f5b5ab8404a24e7753b924c4609480e4cddde
bi1eygr <joseadastra@gmail.com>
2025-01-09 13:30:16 -0500

    Adding metadata

--- LemonLime/nextjs-dashboard/app/layout.tsx
+++ LemonLime/nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Alas Poor Yorick Dashboard',
+    default: 'Alas Poor Yorick Dashboard',
+  },
+  description: 'The official Alas Poor Yorick Dashboard, built with nextJS App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,

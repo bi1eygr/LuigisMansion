bf69061788901324bb3947ff43ca7680987aba1c
bi1eygr <joseadastra@gmail.com>
2025-01-08 21:39:58 -0500

    Deleted multipled defintions of createInvoice

--- LemonLime/nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ LemonLime/nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-import {createInvoice, State } from '@/app/lib/actions';
+import { createInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
@@ -10,7 +10,7 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/actions';
+
 
 export default function Form({ customers }: { customers: CustomerField[]; }) {
     const initialState: State = { message: null, errors: {} };

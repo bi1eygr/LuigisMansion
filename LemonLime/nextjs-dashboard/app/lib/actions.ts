cbf307d5cae5cd3b380c82c0fdb958edcf197ee3
bi1eygr <joseadastra@gmail.com>
2025-01-08 21:21:43 -0500

    Changed safeParose to safeParse

--- LemonLime/nextjs-dashboard/app/lib/actions.ts
+++ LemonLime/nextjs-dashboard/app/lib/actions.ts
@@ -67,7 +67,7 @@ export type State = {
 };
  
 export async function createInvoice(prevState: State, formData: FormData) {
-  const validatedFields = CreateInvoice.safeParase({
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),

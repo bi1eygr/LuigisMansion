48855a4d32ba14f43edab07fb991a340be1f3cdf
bi1eygr <joseadastra@gmail.com>
2025-01-09 10:24:57 -0500

    No way, Jose(Please no one see this and repeat this to me, only I'm allowed to use this phrase on myself

--- LemonLime/nextjs-dashboard/app/lib/actions.ts
+++ LemonLime/nextjs-dashboard/app/lib/actions.ts
@@ -10,26 +10,19 @@ import { redirect } from 'next/navigation';
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
-    required_error: 'Please select a customer.',
+    invalid_type_error: 'Please select a customer.',
   }),
-  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
   status: z.enum(['pending', 'paid'], {
-    required_error: 'Please select an invoice status.',
+    invalid_type_error: 'Please select an invoice status.',
   }),
   date: z.string(),
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
-export type State = {
-  errors?: {
-    customerId?: string[];
-    amount?: string[];
-    status?: string[];
-  };
-  message?: string | null;
-};
 
 export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
